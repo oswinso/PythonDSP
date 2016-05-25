cf061e8a98ecb3fb7ec8cc19a9bcf4fea8f501b7
falafel72 <ksh717@student.cis.edu.hk>
2016-05-26 00:29:25 +0800

    implemented rough version of delay

--- pythondsp/filters/delay.py
+++ pythondsp/filters/delay.py
@@ -1,4 +1,6 @@
 from effect import Effect
+import numpy as np
+
 class Delay(Effect):
 
 	def __init__(self, decay=1, rate=1):
@@ -12,9 +14,12 @@ class Delay(Effect):
 		#PLACEHOLDER
 
 	def applyEffect(self, inputSound):
+		'''
+		#print("hello")
 		delayBuffer = [0] * (int)(self.fs * self.rate)
 		delayBufferPos = 0
 		out = []
+		#print(len(inputSound), len(delayBuffer))
 		for sample in inputSound:
 			# Delay Algorithm
 			output = sample + self.decay * delayBuffer[delayBufferPos]
@@ -23,9 +28,20 @@ class Delay(Effect):
 			# Update Delay Buffer
 			delayBuffer[delayBufferPos] = sample
 			delayBufferPos += 1
-			if(delayBufferPos == self.fs * self.rate):
+			if delayBufferPos == self.fs * self.rate:
+				print("hello")
 				delayBufferPos = 0
-				print("yay")
-		for i in range(0,len(inputSound)):
-			print("{}, {}".format(inputSound[i],out[i]))
+		
+				#print("yay")
+		#for i in range(0,len(inputSound)):
+		#	print("{}, {}".format(inputSound[i],out[i]))
+		'''
+		delaySamples = int(44100/self.rate)
+		out = np.concatenate([inputSound[:],[0]*delaySamples])
+		for i in range(len(out)-delaySamples):
+			out[i + delaySamples] += float(out[i] * self.decay)
+		#for i in range(0,len(inputSound)):
+			#print("{}, {}".format(inputSound[i],out[i]))
+
+		out = np.asarray(out,dtype=np.int16)
 		return out
